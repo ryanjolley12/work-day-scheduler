cb5069def38cb934ad2b07ab8292b95838e03cce
ryanjolley12 <ryanswims@gmail.com>
2021-06-19 12:10:25 -0400

    pseudo codes

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// create h1 element.textcontent append onto body
+// create "time" h2 element, display time here using moment.js , update it by seconds 
+//create object, import moment, assign time to new var 
+// create interval "date/time" setInterval --get time every second and update with new h2
+// don't clear this interval 
+// hourly divs : display time, display input box, and save button
+// createElement div
+// bootstrap for rows/columns 
+// each div has different times/values/ and different save buttons 
+// for loop for variable i=8 to 16
+// create an element div to each element you created
+// add input box when variable i is here time is 08:00 then 09:00 then 1000
+// give ID's for all of the input boxes and save buttons (based on their hours)
+// saveBtn800 saveBtn900
+
+
+// add event listeners to the page 
+// dynamic elements get listened to differently than statically rendered 
+// document.clickEventListener 
+
+// want to do the same thing for every single div 
+// time, box, save button
+
+// document.addEventListener ("onChange", "grab the value of this text box and using the assigne id, create a localStorage the key to which is this id and the value is whatever value is inside this input box")
+// all the divs are similar: put them in a for loop and set TIME based on that for loop 
+// in the same for loop that you're creating time variables need to create other variables as well 
+
+// listen on events, if it changes, go to local storage, grab it and change it 
