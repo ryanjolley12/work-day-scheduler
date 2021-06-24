3ba22513f2ca2b19879231787f44a01e39c75e5d
ryanjolley12 <ryanswims@gmail.com>
2021-06-24 14:48:20 -0400

    localStorage

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,11 @@
 var date = moment();
+console.log(date);
 
 document.getElementById("currentDay").textContent = date; 
-// give each row a # id 
-// if the hour is more than the current time, box is already past 
-
-
-
-
-// find out what the hour value is at the specific time we click 
-// set the row as an hour class 
-// use this.attr to take the value of that number and figure out what the time should be for that block 
-
-// local storage -- okay to repeat each way for retreiving from local storage 
-
-
-// if current time > 9 label as past; if currentTime < 9 label as futre ; if = present 
-
-
 
 var currentTime = moment().format('h:mm');
 console.log(currentTime);
 
-
-
-
-
-
-
-var currentDay = document.createElement("p");
-// currentDay = moment().format("YYYY-MM-DD");
-currentDay.innerHTML = moment().format("MMM DD, YYYY - hh:mm a");
-timeNow.appendChild(currentDay) // Append to <p> with id of timeNow in the HTML doc
-console.log(moment()); //console log because it's a good practice to do so
 
 // WHEN USER CLICKS SAVE, THE TEXT VALUE IS OBTAINED
 $(".saveBtn").on("click", function () {
@@ -44,6 +18,7 @@ $(".saveBtn").on("click", function () {
 });
 
 //MAKE READABLE TIME VARIABLES BUT KEEP NECCESSARY NUMERIC VALUE
+var eight = 8;
 var nine = 9;
 var ten = 10;
 var eleven = 11;
@@ -55,15 +30,32 @@ var four = 16;
 var five = 17;
 
 // OBTAIN DATA FROM LOCALSTORAGE FOR EACH ROW USING ID AND CLASS NAME
-$("#row8 .description").val(localStorage.getItem("row9"));
+$("#row8 .description").val(localStorage.getItem("row8"));
 
 $("#row9 .description").val(localStorage.getItem("row9"));
 
 $("#row10 .description").val(localStorage.getItem("row10"));
 
+$("#row5 .description").val(localStorage.getItem("row5"));
+
+//IF/THEN STATEMENTS: EX. IF currentTime > 9am, label as past; if currentTime < 9am, label as future; if currentTime = 9am, label as now 
 
 
+if (currentHour < nine) {
+    $("#text9").addClass("future");
+} else if (currentHour > nine) {
+    $("#text9").addClass("past");
+} else if (currentHour = nine) {
+    $("#text9").addClass("present");
+}
 
+if (currentHour < five) {
+    $("#text5").addClass("future");
+} else if (currentHour > nine) {
+    $("#text5").addClass("past");
+} else if (currentHour = nine) {
+    $("#text5").addClass("present");
+}
 
 
 
